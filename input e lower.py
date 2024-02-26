0c68c38623a184186c5d51822afe50547b214e24
FireTech-creator <159629143+FireTech-creator@users.noreply.github.com>
2024-02-26 12:02:19 -0300

    Add files via upload

---
+++ input e lower.py
@@ -0,0 +1,8 @@
+    #input me permite colocar uma condição no código.
+nome = input('Digite o seu nome: ')
+
+    #.lower() == -> Esse código me permite concluir a condição mesmo se eu não colocar as letras maisculas corretamente.
+if nome.lower() == 'douglas':
+    print('Que nome lindo você tem!')
+else:
+    print('you have to become more good, this name say weakness')
